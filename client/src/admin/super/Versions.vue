343879671f04ae76992b37b290cb8d406eb6e956
SJYssr <sjyssr@petalmail.com>
2026-05-24 06:46:40 +0800

    fix: 移除版本管理操作列 fixed='right'

--- client/src/admin/super/Versions.vue
+++ client/src/admin/super/Versions.vue
@@ -68,7 +68,7 @@
         </el-table-column>
         <el-table-column prop="created_at" label="创建时间" width="160" />
         <el-table-column prop="updated_at" label="更新时间" width="160" />
-        <el-table-column label="操作" width="160" fixed="right">
+        <el-table-column label="操作" width="160">
           <template #default="{ row }">
             <el-button-group>
               <el-button type="primary" size="small" :icon="Edit" @click="handleEdit(row)">
